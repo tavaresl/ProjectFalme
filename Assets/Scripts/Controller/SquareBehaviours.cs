53206224cbb19e4c529501f7b4c11ea658fe4fb3
Lucas Tavares <lucastavaresdarocha@gmail.com>
2020-07-10 22:24:50 -0300

    initial file structure

---
+++ Assets/Scripts/Controller/SquareBehaviours.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SquareBehaviours : MonoBehaviour
+{
+    public Vector3 Vel { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vel = new Vector3(0.01f, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 acc = new Vector3(0.001f, 0, 0);
+
+        Vel += acc * Time.deltaTime;
+        gameObject.transform.position += Vel;
+    }
+}
