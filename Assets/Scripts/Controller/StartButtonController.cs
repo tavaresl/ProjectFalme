0c212af85fbd393d197720a14744bd356c312641
Diego Garcia <12803744+Dugo03@users.noreply.github.com>
2020-07-12 14:56:29 -0300

    Start Scene

---
+++ Assets/Scripts/Controller/StartButtonController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonController : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("CombatScene");
+    }
+}
